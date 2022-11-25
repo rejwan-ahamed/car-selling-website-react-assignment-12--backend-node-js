98ac4e3888ab1b607096f9601da1f59aadb1cbf8
rejwan-ahamed <rejwanahamed85@gmail.com>
2022-11-25 13:35:23 +0600

    register added

--- index.js
+++ index.js
@@ -4,16 +4,47 @@ const cors = require("cors");
 const port = process.env.PORT || 5000;
 require("dotenv").config();
 
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
 // middle ware
 app.use(cors());
 app.use(express.json());
 
-// get test api
+// get api
 app.get("/", (req, res) => {
   res.send("get api is working");
 });
 
+// mongo db
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.PASSWORD}@cluster0.vhnnfcc.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    // user register data collection
+    const userLoginCollection = client
+      .db("UserRegisterData")
+      .collection("RegisterData");
+
+    app.post("/userRegister", (req, res) => {
+      const userData = req.body;
+      console.log(userData)
+      const result = userLoginCollection.insertOne(userData);
+      res.send(result);
+    });
+  } finally {
+  }
+}
+
+run().catch((error) => {
+  console.log(error);
+});
+
 // api listening
 app.listen(port, () => {
-  console.log("api listening port is", port);
+  console.log(`api listening port is ${port}`);
 });
