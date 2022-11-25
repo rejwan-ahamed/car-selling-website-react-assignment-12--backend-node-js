e6fd53f11a76ab3c20c83ba311a3a9380ba5ff11
rejwan-ahamed <rejwanahamed85@gmail.com>
2022-11-25 12:22:54 +0600

    test api is added

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-const express = require('express');
-const app = express()
-const cors = require('cors');
-const port = process.env.PORT || 5000
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT || 5000;
 require("dotenv").config();
 
 // middle ware
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
+
+// get test api
+app.get("/", (req, res) => {
+  res.send("get api is working");
+});
+
+// api listening
+app.listen(port, () => {
+  console.log("api listening port is", port);
+});
